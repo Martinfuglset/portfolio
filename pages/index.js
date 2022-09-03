7532c8835ec200fbfa90d83c8e68a2324f0831ee
Martin Fuglset <fuglsetm@gmail.com>
2022-09-03 19:31:02 +0200

    change title

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to freaking <a href="https://nextjs.org">Next.js!</a>
+          Portfolio
         </h1>
 
         <p className={styles.description}>
