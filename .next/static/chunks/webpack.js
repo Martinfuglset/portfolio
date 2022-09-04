5c49ec3aaf913be74dd2fff79b2ce1f1d013b458
Martin Fuglset <fuglsetm@gmail.com>
2022-09-04 23:54:53 +0200

    CV module

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ff2961deae37be25"; }
+/******/ 		__webpack_require__.h = function() { return "fef9ce9550d4758d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/cv.js
+++ pages/cv.js
@@ -1,3 +1,49 @@
-export default function cv() {
-    return "hello world"
+import Head from 'next/head'
+
+export default function CV() {
+  return (
+    <div>
+      <Head>
+        <title>Martin Fuglset | CV</title>
+        <meta name="description" content="Created by Martin Fuglset" />
+        <link rel="icon" href="/Vector.ico" />
+      </Head>
+
+      <header>
+        <p>
+          <a href='/'>
+            Martin Fuglset&nbsp;
+          </a>
+          <a>Lights off&nbsp;</a>
+          <a>Menu</a>
+        </p>
+      </header>
+
+      <main>
+        <h1 className="text-6xl font-regular">
+          CV
+        </h1>
+        <button class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+            <form action="https://drive.google.com/file/d/1yGwirkyA8Mwr1J7GoqDUVsQf0Rrzr1QT/view?usp=sharing">
+                <input type="submit" value="Download CV" />
+            </form>
+        </button>
+      
+      </main>
+
+      <footer>
+        <div>
+        <a href='mailto:fuglsetm@gmail.com'>
+          Email&nbsp; 
+        </a>
+        <a href='https://github.com/Martinfuglset'>
+          GitHub&nbsp; 
+        </a>
+        <a href='https://www.linkedin.com/in/martinfuglset/'>
+          LinkedIn
+        </a>
+        </div>
+      </footer>
+    </div>
+  )
 }
